12893bf88d283a5ee96f497030b840996a225bc6
Swinalwaghmare <swinalwaghmare28.com>
2025-11-23 10:20:52 +0530

    Bastion host added

---
+++ Terraform/Module/Bastion/main.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "bastion" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = [var.bastion_security]
+  key_name = var.key_name
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "bastion"
+  }
+}
